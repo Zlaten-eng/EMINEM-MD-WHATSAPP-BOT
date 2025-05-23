1c2345bcbd65a3c908cee1efdff87fd3e7114be9
toxyic-yeng <toxyiclyrikali@gmail.com>
2025-05-24 01:47:52 +0300

    Create advice.js

---
+++ Commands/General/advice.js
@@ -0,0 +1,6 @@
+const advice = require("badadvice");
+
+module.exports = async (context) => {
+        const { client, m, sendReply, sendMediaMessage } = context;
+await sendReply(client, m,advice());
+
